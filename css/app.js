a7e95f41bc8cec1cf4bf9b711b9873bb4799c73b
Ch Farhan <farhanhabibjune2016@gmail.com>
2022-02-24 14:03:51 +0500

    010 How the VueJS Template Syntax and Instance Work Together

--- css/app.js
+++ css/app.js
@@ -1,12 +1,25 @@
 // 003 Extending the VueJS Application
+// new Vue({
+//     el: '#app',
+//     data: {
+//         title: 'Hello World!'
+//     },
+//     methods: {
+//         changeTitle: function(event) {
+//             this.title = event.target.value;
+//         }
+//     }
+// });
+
+// 010 How the VueJS Template Syntax and Instance Work Together
 new Vue({
     el: '#app',
     data: {
         title: 'Hello World!'
     },
     methods: {
-        changeTitle: function(event) {
-            this.title = event.target.value;
+        sayHello: function() {
+            return 'Hello!';
         }
     }
 });
