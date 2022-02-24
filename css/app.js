a1e8010016f47ed94a956070e1566bb362a5fda6
Ch Farhan <farhanhabibjune2016@gmail.com>
2022-02-24 17:48:01 +0500

    019 Modifying an Event - with Event Modifiers

--- css/app.js
+++ css/app.js
@@ -87,6 +87,25 @@
 // });
 
 // 018 Passing your own Arguments with Events
+// new Vue({
+//     el: '#app',
+//     data: {
+//         counter: 0,
+//         x: 0,
+//         y: 0
+//     },
+//     methods: {
+//         increase: function(step, event) {
+//             this.counter += step;
+//         },
+//         updateCoordinates: function(event) {
+//             this.x = event.clientX;
+//             this.y = event.clientY;
+//         }
+//     }
+// });
+
+// 019 Modifying an Event - with Event Modifiers
 new Vue({
     el: '#app',
     data: {
