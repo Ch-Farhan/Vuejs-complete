e923ed20f7794f3ba8eb83f1aa371e041abe672d
Ch Farhan <farhanhabibjune2016@gmail.com>
2022-02-24 18:38:33 +0500

    022 Using Two-Way-Binding

--- css/app.js
+++ css/app.js
@@ -147,23 +147,31 @@
 // });
 
 // 021 Writing JavaScript Code in the Templates
+// new Vue({
+//     el: '#app',
+//     data: {
+//         counter: 0,
+//         x: 0,
+//         y: 0
+//     },
+//     methods: {
+//         increase: function(step, event) {
+//             this.counter += step;
+//         },
+//         updateCoordinates: function(event) {
+//             this.x = event.clientX;
+//             this.y = event.clientY;
+//         },
+//         alertMe: function() {
+//             alert('Alert!');
+//         }
+//     }
+// });
+
+// 022 Using Two-Way-Binding
 new Vue({
     el: '#app',
     data: {
-        counter: 0,
-        x: 0,
-        y: 0
-    },
-    methods: {
-        increase: function(step, event) {
-            this.counter += step;
-        },
-        updateCoordinates: function(event) {
-            this.x = event.clientX;
-            this.y = event.clientY;
-        },
-        alertMe: function() {
-            alert('Alert!');
-        }
+        name: 'Max'
     }
 });
