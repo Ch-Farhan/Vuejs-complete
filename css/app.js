2005a59e7aec5a23b9b8626f77e4b7061bc8772d
Ch Farhan <farhanhabibjune2016@gmail.com>
2022-02-24 17:57:23 +0500

    020 Listening to Keyboard Events

--- css/app.js
+++ css/app.js
@@ -106,6 +106,25 @@
 // });
 
 // 019 Modifying an Event - with Event Modifiers
+// new Vue({
+//     el: '#app',
+//     data: {
+//         counter: 0,
+//         x: 0,
+//         y: 0
+//     },
+//     methods: {
+//         increase: function(step, event) {
+//             this.counter += step;
+//         },
+//         updateCoordinates: function(event) {
+//             this.x = event.clientX;
+//             this.y = event.clientY;
+//         }
+//     }
+// });
+
+// 020 Listening to Keyboard Events
 new Vue({
     el: '#app',
     data: {
@@ -120,6 +139,9 @@ new Vue({
         updateCoordinates: function(event) {
             this.x = event.clientX;
             this.y = event.clientY;
+        },
+        alertMe: function() {
+            alert('Alert!');
         }
     }
 });
