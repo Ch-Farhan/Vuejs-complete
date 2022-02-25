d556a0dd097e7dcbf4e000f7ec9179973b3981ba
Ch Farhan <farhanhabibjune2016@gmail.com>
2022-02-25 12:38:36 +0500

    028 Dynamic Styling with CSS Classes - Using Names

--- css/app.js
+++ css/app.js
@@ -248,10 +248,27 @@
 // });
 
 // 027 Dynamic Styling with CSS Classes - Using Objects
+// new Vue({
+//     el: '#app',
+//     data: {
+//         attachRed: false
+//     },
+//     computed: {
+//         divClasses: function() {
+//             return {
+//                 red: this.attachRed,
+//                 blue: !this.attachRed
+//             };
+//         }
+//     }
+// });
+
+// 028 Dynamic Styling with CSS Classes - Using Names
 new Vue({
     el: '#app',
     data: {
-        attachRed: false
+        attachRed: false,
+        color: 'green'
     },
     computed: {
         divClasses: function() {
