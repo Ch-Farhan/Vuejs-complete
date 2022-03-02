bfc491f95fe786fa8f06211cb5e1370f95548b52
Ch Farhan <farhanhabibjune2016@gmail.com>
2022-03-02 17:39:48 +0500

    093 Registering Components Locally and Globally

--- 07 An Introduction to Components/js/app.js
+++ 07 An Introduction to Components/js/app.js
@@ -1,6 +1,25 @@
 // 092 Storing Data in Components with the Data Method
-// var data = { status: 'Critical' };
-Vue.component('my-cmp', {
+// Vue.component('my-cmp', {
+//     data: function() {
+//         return {
+//             status: 'Critical'
+//         };
+//     },
+//     template: '<p>Server Status: {{ status }} (<button @click="changeStatus">Change</button>) </p>',
+//     methods: {
+//         changeStatus: function() {
+//             this.status = 'Normal';
+//         }
+//     }
+// });
+
+// new Vue({
+//     el: "#app"
+// })
+
+// 093 Registering Components Locally and Globally
+// Vue.component('my-cmp', {})
+var cmp = {
     data: function() {
         return {
             status: 'Critical'
@@ -12,8 +31,15 @@ Vue.component('my-cmp', {
             this.status = 'Normal';
         }
     }
-});
+};
 
 new Vue({
-    el: "#app"
+    el: "#app",
+    components: {
+        'my-cmp': cmp
+    }
+})
+
+new Vue({
+    el: "#app2"
 })
